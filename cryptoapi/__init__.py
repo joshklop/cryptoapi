d6f106acb0fa58d052eac19b15f63484ac8e29ec
Josh Klopfenstein <joshklop10@gmail.com>
2020-08-02 17:57:32 -0500

    Take advantage of `__all__` to make the libary easier to use.

---
+++ cryptoapi/__init__.py
@@ -0,0 +1,28 @@
+from cryptoapi.base.exchange import Exchange
+
+from cryptoapi.bitfinex import Bitfinex
+from cryptoapi.bitvavo import Bitvavo
+from cryptoapi.coinbasepro import Coinbasepro
+from cryptoapi.kraken import Kraken
+
+from cryptoapi.base import errors
+from cryptoapi.base.errors import error_hierarchy
+from cryptoapi.base.errors import SubscribeError
+from cryptoapi.base.errors import UnsubscribeError
+from cryptoapi.base.errors import ChannelLimitExceeded
+from cryptoapi.base.errors import Reconnect
+from cryptoapi.base.errors import UnknownResponse
+
+exchanges = [
+    'Bitfinex',
+    'Bitvavo',
+    'Coinbasepro',
+    'Kraken'
+]
+
+base = [
+    'Exchange',
+    'exchanges',
+]
+
+__all__ = base + errors.__all__ + exchanges
--- cryptoapi/base/errors.py
+++ cryptoapi/base/errors.py
@@ -1,24 +1,65 @@
+from ccxt import errors
 from ccxt.base.errors import ExchangeError
 from ccxt.base.errors import NetworkError
 from ccxt.base.errors import DDoSProtection
 
+
+cryptoapi_errors = [
+    'error_hierarchy',
+    'SubscribeError',
+    'UnsubscribeError',
+    'ChannelLimitExceeed',
+    'Reconnect',
+    'UnkownResponse'
+]
+
+# Do not include ccxt.error_hierarchy
+__all__ = errors.__all__[1:] + cryptoapi_errors
+
 error_hierarchy = {
     'BaseError': {
         'ExchangeError': {
+            'AuthenticationError': {
+                'PermissionDenied': {},
+                'AccountSuspended': {},
+            },
+            'ArgumentsRequired': {},
+            'BadRequest': {
+                'BadSymbol': {},
+            },
+            'BadResponse': {
+                'NullResponse': {},
+            },
+            'InsufficientFunds': {},
+            'InvalidAddress': {
+                'AddressPending': {},
+            },
+            'InvalidOrder': {
+                'OrderNotFound': {},
+                'OrderNotCached': {},
+                'CancelPending': {},
+                'OrderImmediatelyFillable': {},
+                'OrderNotFillable': {},
+                'DuplicateOrderId': {},
+            },
+            'NotSupported': {},
             'SubscribeError': {},
             'UnsubscribeError': {},
         },
         'NetworkError': {
             'DDoSProtection': {
+                'RateLimitExceeded': {},
                 'ChannelLimitExceeed': {},
             },
             'ExchangeNotAvailable': {
                 'OnMaintenance': {},
             },
+            'InvalidNonce': {},
+            'RequestTimeout': {},
             'Reconnect': {},
             'UnkownResponse': {},
-        }
-    }
+        },
+    },
 }
 
 
