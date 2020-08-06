07b9e78f454d30175fb4cdbe1d180aaa99209a42
Josh Klopfenstein <joshklop10@gmail.com>
2020-08-06 14:52:58 -0500

    Update version number.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / 'README.md').read_text()
 
 setup(
     name='cryptoapi',
-    version='0.1.0',
+    version='0.1.1',
     description='Asynchronous cryptocurrency REST and websocket API with support for multiple exchanges.',
     long_description=README,
     long_description_content_type='text/markdown',
